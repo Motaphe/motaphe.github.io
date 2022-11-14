4b8e01dc4a0d45183bb1767d762e2a1a773072f0
Motaphe <suzalregmii@gmail.com>
2022-11-14 01:43:25 -0500

    cleaning-up

--- script.js
+++ script.js
@@ -22,18 +22,22 @@ for (var i=0; themeDots.length > i; i++){
 function setTheme(mode){
 	if(mode == 'light'){
 		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById("github_img").src = "images/github.png"
 	}
 
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById("github_img").src = "images/github_white.png"
 	}
 
 	if(mode == 'green'){
 		document.getElementById('theme-style').href = 'green.css'
+		document.getElementById("github_img").src = "images/github_white.png"
 	}
 
 	if(mode == 'purple'){
 		document.getElementById('theme-style').href = 'purple.css'
+		document.getElementById("github_img").src = "images/github_white.png"
 	}
 
 	localStorage.setItem('theme', mode)
